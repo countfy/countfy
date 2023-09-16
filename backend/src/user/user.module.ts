55aa9c52d310dbb0daab456ffe975edfde22fe37
tomast1337 <vycasnicolas@gmail.com>
2023-09-16 02:51:10 -0300

    exporting UserService for usage in other modules

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -8,5 +8,6 @@ import { User } from './users.entity';
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UserController],
   providers: [UserService],
+  exports: [UserService],
 })
 export class UserModule {}
