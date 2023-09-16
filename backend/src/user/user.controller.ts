1403ca956978717d3fd9b8d33471adb1ce977489
tomast1337 <vycasnicolas@gmail.com>
2023-09-15 23:01:08 -0300

    Created User Entity, service and controller, related to #14

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,4 +1,53 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { User } from './users.entity';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  async createOne(@Body() user: Partial<User>): Promise<User> {
+    return await this.userService.createOne(user);
+  }
+
+  @Get(':id')
+  async readOne(@Param('id') id: string): Promise<User> {
+    return await this.userService.readOne(id);
+  }
+
+  @Get()
+  async readAll(
+    @Query('page') page: number,
+    @Query('size') size: number,
+  ): Promise<User[]> {
+    return await this.userService.readPage(page, size);
+  }
+
+  @Put(':id')
+  async updateOne(
+    @Param('id') id: string,
+    @Body() user: Partial<User>,
+  ): Promise<User> {
+    return await this.userService.updateOne(id, user);
+  }
+
+  @Delete(':id')
+  async deleteOne(@Param('id') id: string): Promise<void> {
+    return await this.userService.deleteOne(id);
+  }
+
+  @Get('email/:email')
+  async readByEmail(@Param('email') email: string): Promise<User> {
+    return await this.userService.readByEmail(email);
+  }
+}
--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,43 +1,51 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { User } from './users.entity';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './users.entity';
 
 @Injectable()
 export class UserService {
-    private readonly logger: Logger = new Logger(UsersService.name);
-    constructor(
-        @InjectableRepository(User)
-        private readonly userRepository: Repository<User>,
-    ) {
-    }
+  private readonly logger: Logger = new Logger(UserService.name);
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
 
-    async createOne(user: Partial<User>): Promise<User> {
-        const newUser = new User();
-        Object.assign(newUser, user);
-        return await newUser.save();
-    }
+  async createOne(user: Partial<User>): Promise<User> {
+    const newUser = new User();
+    Object.assign(newUser, user);
+    return await this.userRepository.save(newUser);
+  }
 
-    async readOne(id: string): Promise<User> {
-        return await User.findOne(id);
-    }
+  async readOne(id: string): Promise<User> {
+    return await this.userRepository
+      .createQueryBuilder('user')
+      .where('user.id = :id', { id })
+      .getOne();
+  }
 
-    async readPage(page: number, size: number): Promise<User[]> {
-        return await User.find({
-            skip: page * size,
-            take: size,
-        });
-    }
+  async readPage(page: number, size: number): Promise<User[]> {
+    return await this.userRepository.find({
+      skip: page * size,
+      take: size,
+    });
+  }
 
-    async updateOne(id: string, user: Partial<User>): Promise<User> {
-        const updatedUser = await User.findOne(id);
-        Object.assign(updatedUser, user);
-        return await updatedUser.save();
-    }
+  async updateOne(id: string, user: Partial<User>): Promise<User> {
+    const updatedUser = await this.readOne(id);
+    Object.assign(updatedUser, user);
+    return await updatedUser.save();
+  }
 
-    async deleteOne(id: string): Promise<void> {
-        await User.delete(id);
-    }
+  async deleteOne(id: string): Promise<void> {
+    await this.userRepository.delete(id);
+  }
 
-    async readByEmail(email: string): Promise<User> {
-        const user = User.
+  async readByEmail(email: string): Promise<User> {
+    const user = this.userRepository
+      .createQueryBuilder('user')
+      .where('user.email = :email', { email })
+      .getOne();
+    return user;
+  }
 }
