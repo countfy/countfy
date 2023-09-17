19b277add55d00185527d04067e0074ed5297c43
tomast1337 <vycasnicolas@gmail.com>
2023-09-17 02:30:14 -0300

    added seed module and changed the configuration of typeorm

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,34 +5,44 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get<string>('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
-        username: configService.get<string>('DB_USER'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_NAME'),
-        autoLoadEntities: true,
-        synchronize: false,
-        dropSchema: false,
-        logging: false,
-        entities: ['dist/**/*.entity{.js,.ts}'],
-        migrations: ['dist/src/migrations/**/*{.js,.ts}'],
-        subscribers: ['dist/src/subscribers/**/*{.js,.ts}'],
-        cli: {
-          migrationsDir: 'src/migrations',
-          subscribersDir: 'src/subscribers',
-        },
-      }),
+      useFactory: (configService: ConfigService) => {
+        const host = configService.get<string>('DB_HOST');
+        const port = configService.get<number>('DB_PORT');
+        const username = configService.get<string>('DB_USER');
+        const password = configService.get<string>('DB_PASSWORD');
+        const database = configService.get<string>('DB_NAME');
+        const nodeEnv = configService.get<string>('NODE_ENV');
+        return {
+          type: 'postgres',
+          host,
+          port,
+          username,
+          password,
+          database,
+          autoLoadEntities: true,
+          synchronize: nodeEnv === 'development',
+          dropSchema: false,
+          logging: false,
+          entities: ['dist/**/*.entity{.js,.ts}'],
+          migrations: ['dist/src/migrations/**/*{.js,.ts}'],
+          subscribers: ['dist/src/subscribers/**/*{.js,.ts}'],
+          cli: {
+            migrationsDir: 'src/migrations',
+            subscribersDir: 'src/subscribers',
+          },
+        };
+      },
     }),
     AuthModule,
     UserModule,
+    SeedModule,
   ],
   controllers: [AppController],
   providers: [AppService],
