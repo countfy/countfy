b004e400ed0730acf71ebe093bf513acc90095c7
Nicolas Vycas Nery <vycasnicolas@gmail.com>
2023-09-21 17:58:28 +0000

    added typeORM module for the project, timer and user-project modules

--- backend/src/timer/timer.module.ts
+++ backend/src/timer/timer.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TimerService } from './timer.service';
 import { TimerController } from './timer.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Timer } from './timer.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Timer])],
   providers: [TimerService],
   controllers: [TimerController],
+  exports: [TimerService],
 })
 export class TimerModule {}
