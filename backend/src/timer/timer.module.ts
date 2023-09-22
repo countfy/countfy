82984055934cab811416af445fd616d08b765ee8
tomast1337 <vycasnicolas@gmail.com>
2023-09-21 21:38:51 -0300

    Created a timer gateway to handle the timer websocket events.

--- backend/src/timer/timer.module.ts
+++ backend/src/timer/timer.module.ts
@@ -3,10 +3,11 @@ import { TimerService } from './timer.service';
 import { TimerController } from './timer.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Timer } from './timer.entity';
+import { TimerGateway } from './timer.gateway';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Timer])],
-  providers: [TimerService],
+  providers: [TimerService,TimerGateway],
   controllers: [TimerController],
   exports: [TimerService],
 })
